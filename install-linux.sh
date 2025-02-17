46f2a00a534bd18a92131ba53f4505d7123fd31a
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-02-16 18:03:39 -0600

    Finish scripts

--- install-linux.sh
+++ install-linux.sh
@@ -1,27 +1,72 @@
 #/bin/bash
 
-sudo apt install curl git tmux wget stow ssh ripgrep -y
+if [ "$(uname)" != "Linux" ]; then
+    echo "This script is intended for Linux only."
+    exit 1
+fi
 
-cd ~/dotfiles
-stow bash bat clean lazygit nvim starship tmux vim
+# Install required packages based on distro
+if [[ "$(uname -r)" == *ARCH* ]]; then
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S --noconfirm curl git tmux wget stow ripgrep yazi lazygit base-devel
+else
+    sudo apt update && sudo apt install -y curl git tmux wget stow ripgrep build-essential
+fi
 
-# Install nvim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+if [ "$PROCESSOR" = "x86_64" ]; then
+    NVIM_ARCH="64"
+elif [ "$PROCESSOR" = "aarch64" ]; then
+    NVIM_ARCH="arm64"
+else
+    echo "Unsupported architecture: $PROCESSOR"
+    exit 1
+fi
+REPO_URL="https://github.com/OrigamiKing3612/dotfiles.git"
+CLONE_DIR="$HOME/dotfiles"
+PROCESSOR=$(uname -m)
+
+# Check if a README file exists (case-insensitive)
+if ! ls README* >/dev/null 2>&1; then
+    echo "Cloning repository..."
+    git clone "$REPO_URL" "$CLONE_DIR"
+fi
+
+cd "$HOME/dotfiles"
+stow bash bat clean lazygit nvim starship tmux vim yazi silicon
+
+# ---------- Installing Stuff ----------
+
+# bat
+bat cache --build
+
+# lazygit
+
+# nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux{$NVIM_ARCH}.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+rm nvim-linux{$NVIM_ARCH}.tar.gz
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
 nvm install 22
 npm install -g tree-sitter-cli
 
-# Install starship
-curl -sS https://starship.rs/install.sh | sh -y
+# starship
+curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-# Install tmux
+# tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Install vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vi +PlugInstall +qall
+# vim (don't install beucase different versions)
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# vi +PlugInstall +qall
+if [[ "$(uname -r)" != *ARCH* ]]; then
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit -D -t /usr/local/bin/
+    rm lazygit.tar.gz
+fi
+
+source ~/.bashrc
