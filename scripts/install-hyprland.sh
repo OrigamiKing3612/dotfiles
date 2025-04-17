5c1d4225ef72aea894c675bfebaa1a40183ce9c3
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-04-16 23:22:30 -0500

    Add swaync config

--- scripts/install-hyprland.sh
+++ scripts/install-hyprland.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S hyprland hypridle hyprlock hyprpaper waybar wofi rofi hyprpolkitagent
+sudo pacman -S hyprland hypridle hyprlock hyprpaper waybar wofi rofi hyprpolkitagent swaync
 yay -S hyprshot
