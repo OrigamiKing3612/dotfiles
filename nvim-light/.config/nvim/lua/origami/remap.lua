b37cab12c9378da6c14377efebebc6e7d9bcd4f0
OrigamiKing3612 <origamiking3612@gmail.com>
2025-09-17 21:17:06 -0500

    fix bugs with nvim-light

--- nvim-light/.config/nvim/lua/origami/remap.lua
+++ nvim-light/.config/nvim/lua/origami/remap.lua
@@ -35,41 +35,11 @@ end)
 -- Renames the word that is under the cursor
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
--- -- tabs
--- vim.keymap.set("n", "<leader>tt", ":tabnew<CR>:Neotree filesystem reveal left<CR>")
--- vim.keymap.set("n", "<leader>tn", ":tabnext<CR>")
--- vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>")
--- vim.keymap.set("n", "<leader>tx", ":tabclose<CR>")
--- vim.keymap.set("n", "<leader>td", ":tabnew %<CR>")
-
 -- tmux
 vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>")
 vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>")
 vim.keymap.set("n", "C-k", ":TmuxNavigateUp<CR>")
 vim.keymap.set("n", "C-l", ":TmuxNavigateRight<CR>")
-
--- spelling
-vim.keymap.set("n", "z=", require("telescope.builtin").spell_suggest)
-vim.keymap.set("n", "zg", "zg") -- add word to dictionary
-vim.keymap.set("n", "zw", "zw") -- remove word from dictionary
-vim.keymap.set("n", "]s", "]s") -- move to next spelling errors
-vim.keymap.set("n", "[s", "[s") -- move to previous spelling errors
-
--- trouble
-vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<CR>", { silent = true, noremap = true })
-vim.keymap.set(
-	"n",
-	"<leader>cl",
-	"<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
-	{ silent = true, noremap = true }
-)
-vim.keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<CR>", { silent = true, noremap = true })
-
--- silicon
-vim.keymap.set({ "v", "x" }, "<leader>sc", "<cmd>'<,'>Silicon<CR>")
 
 vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Wq", "wq", {})
