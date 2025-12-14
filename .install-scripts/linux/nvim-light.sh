fcfef2cd9dec36d56bf90ca2f2539753fecb60d0
OrigamiKing3612 <origamiking3612@gmail.com>
2025-12-14 15:46:29 -0600

    fix another bug

--- .install-scripts/linux/nvim-light.sh
+++ .install-scripts/linux/nvim-light.sh
@@ -19,11 +19,11 @@ esac
 
 sudo apt install -y ripgrep fzf
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux${NVIM_ARCH}.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-${NVIM_ARCH}.tar.gz
 sudo rm -rf /opt/nvim
 sudo mkdir -p /opt/nvim
-sudo tar -xzf nvim-linux${NVIM_ARCH}.tar.gz -C /opt/nvim --strip-components=1
-rm nvim-linux${NVIM_ARCH}.tar.gz
+sudo tar -xzf nvim-linux-${NVIM_ARCH}.tar.gz -C /opt/nvim --strip-components=1
+rm nvim-linux-${NVIM_ARCH}.tar.gz
 
 VERSION=$(curl -s "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | jq -r .tag_name | tr -d '\n')
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${VERSION}/install.sh | bash
