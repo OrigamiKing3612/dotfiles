52361362f6206bafde3bf10f237ba9491c09e21c
OrigamiKing3612 <origamiking3612@gmail.com>
2026-03-29 18:02:25 -0500

    oops

--- nvim/.config/nvim/lua/origami/lazy/comment.lua
+++ nvim/.config/nvim/lua/origami/lazy/comment.lua
@@ -1,16 +1,14 @@
 return {
 	-- 'numToStr/Comment.nvim',
-	"faergeek/Comment.nvim", -- use this and branch until fixed
-	branch = "nvim-0.12-compatibility",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"JoosepAlviste/nvim-ts-context-commentstring",
-	},
-	opts = function()
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-		return {
-			pre_hook = ts_context_commentstring.create_pre_hook(),
-		}
-	end,
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- dependencies = {
+	-- 	"JoosepAlviste/nvim-ts-context-commentstring",
+	-- },
+	-- opts = function()
+	-- 	local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+	--
+	-- 	return {
+	-- 		pre_hook = ts_context_commentstring.create_pre_hook(),
+	-- 	}
+	-- end,
 }
