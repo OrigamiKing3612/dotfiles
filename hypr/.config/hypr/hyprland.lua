b061e480eec68b645961d2041246f2dda2373ef9
OrigamiKing3612 <origamiking3612@gmail.com>
2026-05-17 00:37:22 -0500

    some more updates

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -3,22 +3,23 @@ hl.monitor({
     mode     = "preferred",
     position = "0x0",
     scale    = "1",
+    disabled = true,
 })
 
 hl.monitor({
-    output = "DP-1",
+    output = "DP-2",
     mode = "preferred",
     position = '1920x0',
     scale = "1"
 })
 
 hl.on("hyprland.start", function()
-    -- hl.exec_cmd("nm-applet")
     hl.exec_cmd("hyprpaper & waybar & swaync & hypridle & blueman-applet &")
 end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 require("hyprland.looks")
 require("hyprland.input")
